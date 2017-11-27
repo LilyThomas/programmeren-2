08f4936d126d7f024717b618a042598a053af52f
LilyThomas <anneliesthomas94@gmail.com>
2017-11-27 14:09:45 +0100

    first exercise

---
+++ arrays/Oef1/Oef1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oef1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const int NUMBER_OF = 20;
+            int[] numbers = new int[NUMBER_OF];
+
+            for (int i = 0; i < numbers.Length; i++){
+                numbers[i] = i+1;
+            }
+
+            foreach(int i in numbers){
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+
+            for (int i = 0; i < numbers.Length / 2; i ++){
+                int tmp = numbers[i];
+                numbers[i] = numbers[numbers.Length - i - 1];
+                numbers[numbers.Length - i - 1] = tmp;
+            }
+
+            foreach(int i in numbers){
+                Console.Write(i + " ");
+            }
+
+        }
+    }
+}
---
+++ arrays/Oef2/Oef2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Oef2
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const int NUMBER_OF = 20;
+            int[] numbers = new int[NUMBER_OF];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = i + 1;
+            }
+
+            foreach(int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("");
+
+            for (int i = 0; i < numbers.Length; i ++)
+            {
+                numbers[(i + 1) % numbers.Length] = numbers[i];
+            }
+
+            foreach(int i in numbers){
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
