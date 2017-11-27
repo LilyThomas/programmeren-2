c90bd7cff57fb3236c096000d78b1c94da416aaa
LilyThomas <anneliesthomas94@gmail.com>
2017-11-27 14:32:27 +0100

    arrays oef 2

--- arrays/Oef1/Oef1/Program.cs
+++ arrays/Oef1/Oef1/Program.cs
@@ -9,13 +9,16 @@ namespace Oef1
             const int NUMBER_OF = 20;
             int[] numbers = new int[NUMBER_OF];
 
-            for (int i = 0; i < numbers.Length; i++){
-                numbers[i] = i+1;
+			Console.WriteLine(string.Format("Voeg {0} nummbers in:", NUMBER_OF));
+
+			for (int i = 0; i < NUMBER_OF; i++){
+                numbers[i] = int.Parse(Console.ReadLine());
             }
 
             foreach(int i in numbers){
                 Console.Write(i + " ");
             }
+
             Console.WriteLine("");
 
             for (int i = 0; i < numbers.Length / 2; i ++){
--- arrays/Oef2/Oef2/Program.cs
+++ arrays/Oef2/Oef2/Program.cs
@@ -6,26 +6,30 @@ namespace Oef2
     {
         public static void Main(string[] args)
         {
-            const int NUMBER_OF = 20;
+            const int NUMBER_OF = 5;
             int[] numbers = new int[NUMBER_OF];
+            int[] demo = new int[NUMBER_OF];
 
-            for (int i = 0; i < numbers.Length; i++)
-            {
-                numbers[i] = i + 1;
+            Console.WriteLine(string.Format("Voeg {0} nummers in:", NUMBER_OF));
+
+            for (int i = 0; i < NUMBER_OF; i++){
+                numbers[i] = int.Parse(Console.ReadLine());
             }
 
             foreach(int i in numbers)
             {
                 Console.Write(i + " ");
             }
+
             Console.WriteLine("");
 
-            for (int i = 0; i < numbers.Length; i ++)
-            {
-                numbers[(i + 1) % numbers.Length] = numbers[i];
-            }
+            for (int i = 0; i < numbers.Length; i++)
+			{
+                demo[(i + 1) % numbers.Length] = numbers[i];
+			}
 
-            foreach(int i in numbers){
+
+            foreach(int i in demo){
                 Console.Write(i + " ");
             }
         }
